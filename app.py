9c7ebd9b18c71a460b4a419493a3e95d8dfac6cb
BakedTar <88925785+normalbot98@users.noreply.github.com>
2025-05-15 08:33:49 +0530

    Updated app.py to fix port errors

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from responses import get_response
+import os
 
 app = Flask(__name__)
 
@@ -14,4 +15,5 @@ def chat():
     return jsonify({"response": response})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # use PORT from Render
+    app.run(host="0.0.0.0", port=port)
